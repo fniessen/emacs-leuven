eb45dcf524a6596586e86b5594b12afe94548c2e
Fabrice Niessen <(concat "fniessen" at-sign "pirilampo.org")>
2015-01-08 21:23:36 +0100

    Add a message for " in latex-mode

--- emacs-leuven.el
+++ emacs-leuven.el
@@ -4,7 +4,7 @@
 
 ;; Author: Fabrice Niessen <(concat "fniessen" at-sign "pirilampo.org")>
 ;; URL: https://github.com/fniessen/emacs-leuven
-;; Version: 20150108.2038
+;; Version: 20150108.2116
 ;; Keywords: emacs, dotfile, config
 
 ;;
@@ -72,7 +72,7 @@
 
 ;; This file is only provided as an example.  Customize it to your own taste!
 
-(defconst leuven--emacs-version "20150108.2038"
+(defconst leuven--emacs-version "20150108.2116"
   "Leuven Emacs Config version (date of the last change).")
 
 (message "* --[ Loading Leuven Emacs Config %s]--" leuven--emacs-version)
@@ -3099,6 +3099,7 @@ These packages are neither built-in nor already installed nor ignored."
                       ispell-local-dictionary)
                     (when (boundp 'ispell-dictionary)
                       ispell-dictionary))))
+(message ">>> %s" major-mode)
       (cond
        ((and (string= dict "francais")
              (eq (char-before) ?\")
