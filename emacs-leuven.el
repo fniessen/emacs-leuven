12214becb1e1a8271bdef3064607245186aeea48
Fabrice Niessen <(concat "fniessen" at-sign "pirilampo.org")>
2015-01-23 16:59:38 +0100

    Don't nuke trailing whitespaces in patch files

--- emacs-leuven.el
+++ emacs-leuven.el
@@ -4,7 +4,7 @@
 
 ;; Author: Fabrice Niessen <(concat "fniessen" at-sign "pirilampo.org")>
 ;; URL: https://github.com/fniessen/emacs-leuven
-;; Version: 20150123.0940
+;; Version: 20150123.1658
 ;; Keywords: emacs, dotfile, config
 
 ;;
@@ -72,7 +72,7 @@
 
 ;; This file is only provided as an example.  Customize it to your own taste!
 
-(defconst leuven--emacs-version "20150123.0940"
+(defconst leuven--emacs-version "20150123.1658"
   "Leuven Emacs Config version (date of the last change).")
 
 (message "* --[ Loading Leuven Emacs Config %s]--" leuven--emacs-version)
@@ -1116,10 +1116,13 @@ These packages are neither built-in nor already installed nor ignored."
 
   ;; Nuke all trailing whitespaces in the buffer.
   (add-hook 'before-save-hook
-            (lambda ()
-              ;; Except for Message mode where "-- " is the signature separator
-              ;; (for when using emacsclient to compose emails and doing C-x #).
-              (unless (eq major-mode 'message-mode)
+            (lambda ()                  ; Except for ...
+              (unless (or (eq major-mode 'message-mode)
+                                        ; ... where "-- " is the signature
+                                        ; separator (for when using emacsclient
+                                        ; to compose emails and doing C-x #).
+                          (eq major-mode 'diff-mode))
+                                        ; ... where the diff can't be changed!
                 (delete-trailing-whitespace))))
 
   ;; Visually indicate empty lines after the buffer end in the fringe.
