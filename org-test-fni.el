33e57f5bc3b674a3d0332f38a080faceb543b14d
Fabrice Niessen <(concat "fniessen" at-sign "pirilampo.org")>
2015-01-30 15:41:28 +0100

    Improve documentation of main function

--- org-test-fni.el
+++ org-test-fni.el
@@ -31,45 +31,78 @@
 
 ;;; Functions for writing tests
 
-(defun compare-org-html-export-files (orgfile)
-  "Compare current export of ORGFILE with HTML file already present on disk."
-  (let* ((base-name
-          (concat (file-name-directory orgfile) (file-name-base orgfile)))
-         (htmlfile (concat base-name ".html"))
-         htmlcontents)
-    (should
-     (equal
-      ;; new export
-      (with-temp-buffer
-        (insert-file-contents orgfile)
-        (setq htmlcontents (org-export-as 'html))
-        (delete-region (point-min) (point-max))
-        (insert htmlcontents)
-        (buffer-string))
-      ;; old export
-      (with-temp-buffer
-        (insert-file-contents htmlfile)
-        (buffer-string))))))
+(defun compare-org-html-export-files (org-file)
+  "Compare current export of ORG-FILE with HTML file already present on disk."
+  (require 'ox-html)
+  (let* ((html-file (concat (file-name-directory org-file)
+                            (file-name-base org-file) ".html"))
+         html-contents)
+    ;; should have a .html file
+    (should (file-exists-p html-file))
+    ;; should have the same .html exported file
+    (should (equal
+             ;; new export
+             (with-temp-buffer
+               (insert-file-contents org-file)
+               (setq html-contents (org-export-as 'html))
+               (delete-region (point-min) (point-max))
+               (insert html-contents)
+               (buffer-string))
+             ;; old export
+             (with-temp-buffer
+               (insert-file-contents html-file)
+               (buffer-string))))))
 
 ;;; Internal Tests
 
 (ert-deftest test-org-export/export-html-backend-test-file ()
-  "Compare current export of ORGFILE with HTML file already present on disk."
+  "Compare current export of ORG-FILE with HTML file already present on disk."
   (compare-org-html-export-files "~/src/emacs-leuven/org-test-sample.org"))
 
 ;; (ert 'test-org-export/export-html-backend-test-file)
 
 (ert-deftest test-org-export/export-html-backend-example-file ()
-  "Compare current export of ORGFILE with HTML file already present on disk."
+  "Compare current export of ORG-FILE with HTML file already present on disk."
   (compare-org-html-export-files "~/src/org-style/example.txt"))
 
 ;; (ert 'test-org-export/export-html-backend-example-file)
 
 (ert-deftest test-org-export/export-html-backend-ERT-refcard-file ()
-  "Compare current export of ORGFILE with HTML file already present on disk."
+  "Compare current export of ORG-FILE with HTML file already present on disk."
   (compare-org-html-export-files "~/src/reference-cards/ERT-refcard.txt"))
 
 ;; (ert 'test-org-export/export-html-backend-ERT-refcard-file)
+
+;; For inspiration:
+;;
+;; (ert-deftest test-ob-exp/org-babel-exp-src-blocks/w-no-headers ()
+;;   "Testing export without any headlines in the Org mode file."
+;;   (require 'ox-html)
+;;   (let ((html-file (concat (file-name-sans-extension org-test-no-heading-file)
+;; 			   ".html")))
+;;     (when (file-exists-p html-file) (delete-file html-file))
+;;     (org-test-in-example-file org-test-no-heading-file
+;;       ;; Export the file to HTML.
+;;       (org-export-to-file 'html html-file))
+;;     ;; should create a .html file
+;;     (should (file-exists-p html-file))
+;;     ;; should not create a file with "::" appended to it's name
+;;     (should-not (file-exists-p (concat org-test-no-heading-file "::")))
+;;     (when (file-exists-p html-file) (delete-file html-file))))
+;;
+;; (ert-deftest test-ob-exp/org-babel-exp-src-blocks/w-no-file ()
+;;   "Testing export from buffers which are not visiting any file."
+;;   (require 'ox-html)
+;;   (let ((name (generate-new-buffer-name "*Org HTML Export*")))
+;;     (org-test-in-example-file nil
+;;       (org-export-to-buffer 'html name nil nil nil t))
+;;     ;; Should create a HTML buffer.
+;;     (should (buffer-live-p (get-buffer name)))
+;;     ;; Should contain the content of the buffer.
+;;     (with-current-buffer (get-buffer name)
+;;       (should (string-match (regexp-quote org-test-file-ob-anchor)
+;; 			    (buffer-string))))
+;;     (when (get-buffer name) (kill-buffer name))))
 
 (provide 'org-test-fni)
 
