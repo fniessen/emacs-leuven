f81ef50b4adfeebc65da5148a7358e63a0ed595b
Fabrice Niessen <(concat "fniessen" at-sign "pirilampo.org")>
2015-06-19 09:23:30 +0200

    Add binding for `C-x 8 0' (in a protected block)

--- emacs-leuven.el
+++ emacs-leuven.el
@@ -4,7 +4,7 @@
 
 ;; Author: Fabrice Niessen <(concat "fniessen" at-sign "pirilampo.org")>
 ;; URL: https://github.com/fniessen/emacs-leuven
-;; Version: 20150618.1659
+;; Version: 20150619.0920
 ;; Keywords: emacs, dotfile, config
 
 ;;
@@ -72,7 +72,7 @@
 
 ;; This file is only provided as an example.  Customize it to your own taste!
 
-(defconst leuven--emacs-version "20150618.1659"
+(defconst leuven--emacs-version "20150619.0920"
   "Leuven Emacs Config version (date of the last change).")
 
 (message "* --[ Loading Leuven Emacs Config %s]--" leuven--emacs-version)
@@ -3038,8 +3038,11 @@ These packages are neither built-in nor already installed nor ignored."
 
   (leuven--section "22.1 (emacs)International Chars")
 
-  ;; ;; Add binding for "zero width space".
-  ;; (define-key iso-transl-ctl-x-8-map (kbd "0") [?​])
+  ;; Keyboard input definitions for ISO 8859-1.
+  (with-eval-after-load "iso-transl"
+
+    ;; Add binding for "zero width space".
+    (define-key iso-transl-ctl-x-8-map (kbd "0") [?​]))
 
 ;;** 22.4 (info "(emacs)Input Methods")
 
