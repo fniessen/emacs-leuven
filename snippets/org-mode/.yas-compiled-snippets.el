0af1593811c18d7802567a05d978a91e91e6eb1b
Fabrice Niessen <fniessen@pirilampo.org>
2018-08-15 12:21:19 +0200

    Add snippets for Message and Org modes

---
+++ snippets/org-mode/.yas-compiled-snippets.el
@@ -0,0 +1,10 @@
+;;; Compiled snippets and support files for `org-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'org-mode
+                     '(("loremipsum3" "Phasellus ut libero. Nulla in libero non enim tristique sollicitudin. Ut\ntempor. Phasellus pellentesque augue eget ante. Mauris malesuada. Donec sit\namet diam sit amet dolor placerat blandit. Morbi enim purus, imperdiet in,\nmolestie sit amet, pellentesque eu, mauris. In vel erat vel ipsum bibendum\ncommodo. Curabitur accumsan. Nam sed metus. Etiam tristique bibendum justo.\n" "Phasellus ut..." nil nil nil "/cygdrive/d/Users/fni/2-Output/emacs-leuven/snippets/org-mode/loremipsum3.yasnippet" nil nil)
+                       ("loremipsum2" "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac\nturpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor\nsit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies\nmi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien\nullamcorper pharetra. Vestibulum erat wisi..." "Pellentesque habitant..." nil nil nil "/cygdrive/d/Users/fni/2-Output/emacs-leuven/snippets/org-mode/loremipsum2.yasnippet" nil nil)
+                       ("loremipsum" "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum." "Lorem ipsum..." nil nil nil "/cygdrive/d/Users/fni/2-Output/emacs-leuven/snippets/org-mode/loremipsum.yasnippet" nil nil)))
+
+
+;;; Do not edit! File generated at Tue Aug 30 10:46:23 2016
