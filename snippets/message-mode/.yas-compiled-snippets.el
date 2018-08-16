f11c3831345027bb4bd3e6e26aa580541d994e80
Fabrice Niessen <fniessen@pirilampo.org>
2018-08-16 12:23:41 +0200

    Tangle snippets to ~/.emacs.d/snippets

--- snippets/message-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `message-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'message-mode
-                     '(("br" "Best regards,\n" "Best regards," nil nil nil "/cygdrive/d/Users/fni/2-Output/emacs-leuven/snippets/message-mode/br.yasnippet" nil nil)))
-
-
-;;; Do not edit! File generated at Tue Aug 30 10:46:23 2016
