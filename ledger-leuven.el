c576a80f0e21f1f7c864cae0c075060f2f53e623
Fabrice Niessen <fniessen@pirilampo.org>
2020-12-12 12:04:13 +0100

    Reindent

---
+++ ledger-leuven.el
@@ -0,0 +1,32 @@
+  ;; Packages to be installed by the user.
+  (add-to-list 'package-selected-packages 'ledger-mode)
+
+  ;; Helper code for use with the "ledger" command-line tool.
+  (add-to-list 'auto-mode-alist '("\\.dat\\'"     . ledger-mode))
+  (add-to-list 'auto-mode-alist '("\\.journal\\'" . ledger-mode))
+  (add-to-list 'auto-mode-alist '("\\.ledger\\'"  . ledger-mode))
+
+  (with-eval-after-load "ledger-commodities"
+
+    ;; Default commodity for use in target calculations in ledger reconcile.
+    (setq ledger-reconcile-default-commodity "EUR")) ; "€"
+
+  ;; Provide custom fontification for ledger-mode.
+  (with-eval-after-load "ledger-fontify"
+
+    ;; If t, the highlight entire xact with state.
+    (setq ledger-fontify-xact-state-overrides nil))
+                                        ; Don't override the highlighting of
+                                        ; each posted item in a xact if it is
+                                        ; cleared/pending. XXX
+
+  (with-eval-after-load "ledger-init"
+
+    ;; (setq ledger-default-date-format "%Y-%m-%d")
+    (setq ledger-default-date-format "%Y/%m/%d")
+    )
+
+  (with-eval-after-load "flycheck"
+
+    ;; Flycheck integration for ledger files.
+    (try-require 'flycheck-ledger))
