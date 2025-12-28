31b3a70360f701206fc93adc9e2a2a5710f7baf0
Fabrice Niessen <fni@pirilampo.be>
2025-12-28 13:49:11 +0100

    Ignore LaTeX build artifacts in completion (Dired-x)
    After loading 'dired-x', add common LaTeX build artifact extensions
    ('.fdb_latexmk', '.fls', '.nav', '.out', '.snm', '.swp', '.vrb') to
    'completion-ignored-extensions'.

--- lisp/emacs-leuven.el
+++ lisp/emacs-leuven.el
@@ -4,7 +4,7 @@
 
 ;; Author: Fabrice Niessen <(concat "fniessen" at-sign "pirilampo.org")>
 ;; URL: https://github.com/fniessen/emacs-leuven
-;; Version: <20251223.1826>
+;; Version: <20251228.1347>
 ;; Keywords: emacs, dotfile, config
 
 ;;
@@ -53,7 +53,7 @@
 ;; This file is only provided as an example. Customize it to your own taste!
 
 ;; Define the version as the current timestamp of the last change.
-(defconst boost-version "<20251223.1826>"
+(defconst boost-version "<20251228.1347>"
   "Version of Emacs-Leuven configuration.")
 
 ;; Announce the start of the loading process.
@@ -6448,6 +6448,10 @@ This example lists Azerty layout second row keys."
     (leuven--section "30.XX (dired-x)Top")
 
     (require 'dired-x)                  ; with-eval-after-load "dired" ends here.
+
+    ;; Ignore LaTeX build artifacts in completion.
+    (dolist (ext '(".fdb_latexmk" ".fls" ".nav" ".out" ".snm" ".swp" ".vrb"))
+      (add-to-list 'completion-ignored-extensions ext))
 
     (when lvn--cygwin-p
       (when (fboundp 'dired-jump)       ; Check if `dired-jump' exists.
