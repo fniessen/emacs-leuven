03d9cd14edf563c533e35aec8ca2113278a0f1b5
Fabrice Niessen <fni@pirilampo.be>
2026-01-18 17:46:04 +0100

    Switch org-latex class to KOMA-Script article with custom options
    - Rename the default Org LaTeX class from “scrartcl” to “koma-article”
    - Introduce '[NO-DEFAULT-PACKAGES]' and '[EXTRA]' tags in the '\documentclass'
      line to allow finer control over package loading
    - Apply the change consistently in both the documentation
      (docs/emacs-leuven-org.txt) and the implementation (lisp/emacs-leuven-org.el)

--- lisp/emacs-leuven-org.el
+++ lisp/emacs-leuven-org.el
@@ -2038,13 +2038,15 @@ buffer."
   (setq org-latex-default-figure-position "!htbp")
 
 (add-to-list 'org-latex-classes
-             '("scrartcl"
-               "\\documentclass{scrartcl}"
-               ("\\section{%s}" . "\\section*{%s}")
-               ("\\subsection{%s}" . "\\subsection*{%s}")
-               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-               ("\\paragraph{%s}" . "\\paragraph*{%s}")
-               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+          '("koma-article"
+             "\\documentclass{scrartcl}
+             [NO-DEFAULT-PACKAGES]
+             [EXTRA]"
+             ("\\section{%s}" . "\\section*{%s}")
+             ("\\subsection{%s}" . "\\subsection*{%s}")
+             ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+             ("\\paragraph{%s}" . "\\paragraph*{%s}")
+             ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
 
   (defun leuven--org-export-ignore-headlines (data backend info)
     "Remove headlines tagged \"ignore\" retaining contents and promoting children.
